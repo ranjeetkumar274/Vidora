a015e2097b6953103f9de9e6337352e5279be894
ranjeetkumar274 <2023073045@mmmut.ac.in>
2025-01-17 16:27:08 +0530

    updated

--- client/webpack-dev.config.js
+++ client/webpack-dev.config.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
